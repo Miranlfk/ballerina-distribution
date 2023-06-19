fd0ea3a45e9a7a69dacac6d865fb354cd03093a7
Miran Kurukulasuriya <miran@wso2.com>
2023-06-19 13:50:27 +0530

    Test python script

--- docs/update_version.py
+++ docs/update_version.py
@@ -1,15 +1,16 @@
+import re
 import os
 
 markdown_file = 'release_notes.md'
-version_placeholder = 'version'
-new_version = os.getenv('NEW_VERSION')
+version_placeholder = r'{{Version}}'
+new_version = os.environ.get('NEW_VERSION')
 
-with open(markdown_file, 'r') as file:
-    content = file.read()
+with open(markdown_file, 'r', encoding='utf8') as file:
+    data = file.read()
 
-updated_content = content.replace(version_placeholder, new_version)
+updated_content = re.sub(version_placeholder, new_version, data)
 
-with open(markdown_file, 'w') as file:
+with open(markdown_file, 'w', encoding='utf8') as file:
     file.write(updated_content)
 
 print('Markdown file updated successfully!')
